2722cbe5782537f450c8391e2b8dfbb5856ba064
Benjamin Bolte <ben@bolte.cc>
2022-11-11 02:34:55 -0500

    fix async iterable (#25)

--- ml/tasks/datasets/async_iterable.py
+++ ml/tasks/datasets/async_iterable.py
@@ -1,38 +1,41 @@
+from __future__ import annotations
+
 import asyncio
+import logging
 import queue
 import threading
-from typing import AsyncIterator, Iterator, Optional, TypeVar
+from typing import AsyncIterator, Iterator, TypeVar
 
 from torch.utils.data.dataset import IterableDataset
+
+logger = logging.getLogger(__name__)
 
 T = TypeVar("T")
 
 
-def iter_async_items(
-    async_iter: AsyncIterator[T],
-    loop: asyncio.AbstractEventLoop,
-    max_queue_size: int,
-) -> Iterator[T]:
-    q: "queue.Queue[Optional[T]]" = queue.Queue(maxsize=max_queue_size)
+async def add_to_queue(async_iter: AsyncIterator[T], q: "queue.Queue[T | None]") -> None:
+    try:
+        async for item in async_iter:
+            assert item is not None, "Item should not be None"
+            q.put(item)
+    finally:
+        q.put(None)
 
-    async def async_iter_to_queue() -> None:
-        try:
-            async for item in async_iter:
-                q.put(item)
-        finally:
-            q.put(None)
 
-    async_result = asyncio.run_coroutine_threadsafe(async_iter_to_queue(), loop)
+def thread_worker(async_iter: AsyncIterator[T], q: "queue.Queue[T | None]") -> None:
+    asyncio.run(add_to_queue(async_iter, q))
 
-    def yield_queue_items() -> Iterator[T]:
-        while True:
-            next_item = q.get()
-            if next_item is None:
-                break
-            yield next_item
-        async_result.result()
 
-    return yield_queue_items()
+def thread_async_iter(async_iter: AsyncIterator[T], max_queue_size: int) -> Iterator[T]:
+    q: "queue.Queue[T | None]" = queue.Queue(maxsize=max_queue_size)
+    thread = threading.Thread(target=thread_worker, args=(async_iter, q), daemon=True)
+    thread.start()
+    while True:
+        item = q.get(block=True)
+        if item is None:
+            break
+        yield item
+    thread.join()
 
 
 class AsyncIterableDataset(IterableDataset[T]):
@@ -43,17 +46,8 @@ class AsyncIterableDataset(IterableDataset[T]):
         # elements, in order to avoid having the queue get too large.
         self.max_async_queue_size = max_async_queue_size
 
-        # Placeholders for the async loop and thread.
-        self.loop: Optional[asyncio.AbstractEventLoop] = None
-        self.thread: Optional[threading.Thread] = None
-
     def __aiter__(self) -> AsyncIterator[T]:
         raise NotImplementedError
 
     def __iter__(self) -> Iterator[T]:
-        if self.loop is None:
-            self.loop = asyncio.get_event_loop()
-        if self.thread is None:
-            self.thread = threading.Thread(target=self.loop.run_forever, daemon=True)
-            self.thread.start()
-        return iter_async_items(self.__aiter__(), self.loop, self.max_async_queue_size)
+        return thread_async_iter(self.__aiter__(), self.max_async_queue_size)
