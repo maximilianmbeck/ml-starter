138d66f9066b9bdaf9a17cf4eb7a84a602891306
Benjamin Bolte <ben@bolte.cc>
2022-12-16 05:45:47 -0500

    improve `get_stem` (#56)
    * improve `get_stem`
    
    * Update cli.py
    
    * Update cli.py

--- ml/utils/cli.py
+++ ml/utils/cli.py
@@ -32,6 +32,20 @@ def get_exp_name(prefix: Optional[str] = None, args: Optional[List[str]] = None)
     return ".".join(p for p in parts if p)
 
 
+def get_stem(path_str: str) -> str:
+    path = Path(path_str).resolve()
+
+    # Remove the `.yaml` suffix.
+    path = path.parent / path.stem
+
+    # Special handling for paths that are relative to the configs directory.
+    for parent in path.parents:
+        if parent.stem == "configs":
+            return ".".join(path.relative_to(parent).parts)
+
+    return path.stem
+
+
 def parse_cli(args: List[str]) -> DictConfig:
     """Parses the remaining command-line arguments to a raw config.
 
@@ -51,7 +65,6 @@ def parse_cli(args: List[str]) -> DictConfig:
 
     # Builds the configs from the command-line arguments.
     config = DictConfig({})
-    get_stem = lambda new_path: Path(new_path).stem
     argument_parts: List[str] = []
     paths: List[Path] = []
 
