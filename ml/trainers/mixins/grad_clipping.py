Gradient clipping fixes (#237)

ml/trainers/mixins/grad_clipping.py
@@ -21,10 +21,9 @@ greater than some threshold.
 """
 
 from dataclasses import dataclass
-from typing import Any, Callable, TypeVar
+from typing import Any, TypeVar
 
-import torch
-from torch import Tensor, nn
+from torch import nn
 from torch.distributed.fsdp.fully_sharded_data_parallel import FullyShardedDataParallel as FSDP
 from torch.optim import Optimizer
 
@@ -39,10 +38,8 @@ from ml.trainers.mixins.mixed_precision import (
 @dataclass
 class GradientClipping:
     clip_grad_norm: float | None = conf_field(None, help="What to clip the gradient norm to")
-    norm_type: Any = conf_field(2, help="Type of norm to use")
     clip_grad_value: float | None = conf_field(None, help="What to clip the gradient value to")
-    clip_global_grad_norm: float | None = conf_field(None, help="What to clip global gradient norm to")
-    log_grad: bool = conf_field(False, help="Whether to log the gradient norm")
+    norm_type: Any = conf_field(2, help="Type of norm to use")
 
 
 @dataclass
@@ -53,52 +50,26 @@ class GradientClippingConfig(MixedPrecisionTrainerConfig, BaseTrainerConfig):
 GradientClippingConfigT = TypeVar("GradientClippingConfigT", bound=GradientClippingConfig)
 
 
-def get_clip_grad_func(clip_value: float) -> Callable[[Tensor], Tensor]:
-    def func(grad: Tensor) -> Tensor:
-        return grad.clamp(-clip_value, clip_value)
-
-    return func
-
-
-def get_clip_norm_func(clip_value: float, norm_type: Any) -> Callable[[Tensor], Tensor]:
-    def func(grad: Tensor) -> Tensor:
-        grad_norm = torch.norm(grad, p=norm_type)
-        return grad * (grad_norm.clamp_max(clip_value) / grad_norm)
-
-    return func
-
-
 class GradientClippingTrainerMixin(
     MixedPrecisionTrainerMixin[GradientClippingConfigT, ModelT, TaskT],
     BaseTrainer[GradientClippingConfigT, ModelT, TaskT],
 ):
     """Defines a trainer mixin for doing gradient clipping."""
 
-    def maybe_add_grad_clipping(self, model: nn.Module) -> None:
-        clip_value = self.config.grad_clipping.clip_grad_value
-        clip_norm = self.config.grad_clipping.clip_grad_norm
-        if clip_value is not None:
-            for p in model.parameters():
-                if p.requires_grad:
-                    p.register_hook(get_clip_grad_func(clip_value))
-        if clip_norm is not None:
-            for p in model.parameters():
-                if p.requires_grad:
-                    p.register_hook(get_clip_norm_func(clip_norm, self.config.grad_clipping.norm_type))
-
     def clip_grads(self, model: nn.Module, optim: Optimizer) -> None:
-        clip_norm = self.config.grad_clipping.clip_global_grad_norm
+        clip_norm = self.config.grad_clipping.clip_grad_norm
+        clip_value = self.config.grad_clipping.clip_grad_value
         norm_type = self.config.grad_clipping.norm_type
-        log_grad = self.config.grad_clipping.log_grad
-        if isinstance(model, FSDP):
-            if clip_norm is not None:
+
+        if clip_norm is not None:
+            if isinstance(model, FSDP):
                 total_norm = model.clip_grad_norm_(clip_norm, norm_type)
                 self.logger.log_scalar("total_norm", total_norm.item(), namespace="optim")
-        elif clip_norm is not None:
+            else:
+                self.unscale_mixed_precision(optim)
+                total_norm = nn.utils.clip_grad.clip_grad_norm_(model.parameters(), clip_norm, norm_type)
+                self.logger.log_scalar("total_norm", total_norm.item(), namespace="optim")
+
+        if clip_value is not None:
             self.unscale_mixed_precision(optim)
-            total_norm = nn.utils.clip_grad.clip_grad_norm_(model.parameters(), clip_norm, norm_type)
-            self.logger.log_scalar("total_norm", total_norm.item(), namespace="optim")
-        elif log_grad:
-            self.unscale_mixed_precision(optim)
-            total_grad = sum(param.grad.norm(norm_type) ** 2 for param in model.parameters() if param.grad is not None)
-            self.logger.log_scalar("total_grad", total_grad.item(), namespace="optim")
+            nn.utils.clip_grad.clip_grad_value_(model.parameters(), clip_value)